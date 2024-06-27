7897371d799e9c30c1aaeb2913515b102bd28375
CongDungIT21 <84309393+CongDungIT21@users.noreply.github.com>
2024-06-27 16:21:28 +0700

    Feature PlayerPrefabs

--- Assets/__Code/Scripts/PlayerDataManager.cs
+++ Assets/__Code/Scripts/PlayerDataManager.cs
@@ -1,6 +1,55 @@
 using Mul21_Lib;
+using Newtonsoft.Json;
+using UnityEngine;
 
 public class PlayerDataManager : PersistentMonoSingleton<PlayerDataManager> 
 {
-    private PlayerData PlayerData;
+    private const string KEY_PLAYER = "PLAYER_DATA";
+    private PlayerData _PlayerData;
+
+    public PlayerData GetPlayerData()
+    {
+        if(_PlayerData != null)
+        {
+            return _PlayerData;
+        }
+
+        _PlayerData = LoadPlayerData();
+        return _PlayerData;
+    }
+
+    public void SavePlayerData(PlayerData playerData)
+    {
+        SavePlayerDataFromPlayerPrefab(playerData);
+    }
+
+    private PlayerData LoadPlayerData()
+    {
+        return LoadPlayDataFromPlayerPrefab();
+    }
+
+    #region PlayerPrefab Load
+    private PlayerData LoadPlayDataFromPlayerPrefab()
+    {
+        string txtData = PlayerPrefs.GetString(KEY_PLAYER);
+
+        if(string.IsNullOrEmpty(txtData))
+        {
+            return null;
+        }
+
+        return JsonConvert.DeserializeObject<PlayerData>(txtData.Trim());
+    }
+
+    private void SavePlayerDataFromPlayerPrefab(PlayerData data)
+    {
+        PlayerPrefs.SetString(KEY_PLAYER, JsonConvert.SerializeObject(data));
+    }
+
+    private void DeletePlayerDataFromPlayerPrefab()
+    {
+        PlayerPrefs.DeleteKey(KEY_PLAYER);
+    }
+
+    #endregion PlayerPrefab Load
 }
