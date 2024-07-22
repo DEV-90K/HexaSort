e88220cd00e7651201261a8bd68f400bb5892910
CongDungIT21 <84309393+CongDungIT21@users.noreply.github.com>
2024-07-22 15:15:32 +0700

    Refactor Folder Scripts

---
+++ Assets/Scripts/Data/ChestRewardData.cs
@@ -0,0 +1,15 @@
+public enum RewardType
+{
+    NONE,
+    COIN,
+    MATERIAL,
+    HAMMER,
+    REFRESH,
+    SWAP
+}
+
+public class ChestRewardData
+{
+    public RewardType Type;
+    public int Amount;
+}
